6a811af195a533d4a267a8eda43c0844b9250b6c
fcx <2196992498@qq.com>
2021-10-31 21:06:25 +0800

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function gravatar($size = '100')
+    {
+    $hash = md5(strtolower(trim($this->attributes['email'])));
+    return " https://gravatar.zeruns.tech/avatar/$hash?s=$size";
+    // return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
