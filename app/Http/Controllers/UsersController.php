e6fc19515c88566eb9a182ae9f56470c78c7fe63
fcx <2196992498@qq.com>
2021-10-31 22:09:19 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,7 +1,10 @@
 <?php
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
+
 class UsersController extends Controller
 {
 public function create()
@@ -26,6 +29,7 @@ $user = User::create([
 'password' => bcrypt($request->password),
 ]);
 
+Auth::login($user);
 session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 return redirect()->route('users.show', [$user]);
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -13,5 +13,6 @@
 @include('layouts._footer')
 </div>
 </div>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
-</html>
+</html>
