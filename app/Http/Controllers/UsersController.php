63922586b61db42bc46bdb17a4e7c77548d4260e
fcx <2196992498@qq.com>
2021-10-31 21:18:16 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,21 @@ use Illuminate\Http\Request;
 use App\Models\User;
 class UsersController extends Controller
 {
-public function create(){
+public function create()
+{
 return view('users.create');
 }
 public function show(User $user)
 {
 return view('users.show', compact('user'));
+}
+public function store(Request $request)
+{
+$this->validate($request, [
+'name' => 'required|unique:users|max:50',
+'email' => 'required|email|unique:users|max:255',
+'password' => 'required|confirmed|min:6'
+]);
+return;
 }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,13 +8,16 @@
 </div>
 <div class="card-body">
 <form method="POST" action="{{ route('users.store') }}">
+{{ csrf_field() }}
 <div class="form-group">
 <label for="name">名称：</label>
-<input type="text" name="name" class="form-control" value="{{old('name') }}">
+<input type="text" name="name" class="form-control" value="{{
+old('name') }}">
 </div>
 <div class="form-group">
 <label for="email">邮箱：</label>
-<input type="text" name="email" class="form-control" value="{{old('email') }}">
+<input type="text" name="email" class="form-control" value="{{
+old('email') }}">
 </div>
 <div class="form-group">
 <label for="password">密码：</label>
@@ -30,4 +33,4 @@ value="{{ old('password') }}">
 </div>
 </div>
 </div>
-@stop
+@stop
