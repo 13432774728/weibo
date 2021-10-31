fba584e51911bb8985cd6f5cf85ae881e568f6f4
fcx <2196992498@qq.com>
2021-10-31 20:19:37 +0800

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,11 +4,13 @@
 <h1>Hello Laravel</h1>
 <p class="lead">
 你现在所看到的是 <a href="https://learnku.com/courses/laravel-essentialtraining">Laravel 入门教程</a> 的示例项目主页。
-</p><p>
+</p>
+<p>
 一切，将从这里开始。
 </p>
 <p>
-<a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+<a class="btn btn-lg btn-success" href="{{ route('signup') }}"
+role="button">现在注册</a>
 </p>
 </div>
 @stop
