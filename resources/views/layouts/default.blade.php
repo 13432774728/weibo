2f1417ec89d8520a86288bf00d884f28d9b102eb
fcx <2196992498@qq.com>
2021-10-31 19:52:45 +0800

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,17 +5,12 @@
 <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+@include('layouts._header')
 <div class="container">
-<a class="navbar-brand" href="/">Weibo App</a>
-<ul class="navbar-nav justify-content-end">
-<li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-<li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-</ul>
-</div>
-</nav>
-<div class="container">
+<div class="offset-md-1 col-md-10">
 @yield('content')
+@include('layouts._footer')
+</div>
 </div>
 </body>
-</html>
+</html>
