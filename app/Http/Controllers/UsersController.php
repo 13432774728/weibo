0f7fe57a56f2fe0f17fd8e3a98bdabedb6481898
fcx <2196992498@qq.com>
2021-11-02 10:53:09 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,59 +5,64 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
 
-class UsersController extends Controller
-{
+class UsersController extends Controller{
     public function __construct()
     {
     $this->middleware('auth', [
-    'except' => ['show', 'create', 'store']
+    'except' => ['show', 'create', 'store', 'index']
     ]);
     }
-public function create()
-{
-return view('users.create');
-}
-public function show(User $user)
-{
-return view('users.show', compact('user'));
-}
-public function store(Request $request)
-{
-$this->validate($request, [
-'name' => 'required|unique:users|max:50',
-'email' => 'required|email|unique:users|max:255',
-'password' => 'required|confirmed|min:6'
-]);
 
-$user = User::create([
-'name' => $request->name,
-'email' => $request->email,
-'password' => bcrypt($request->password),
-]);
+    public function index(){
+    // $users = User::all();
+    $users = User::paginate(6);
+    return view('users.index', compact('users'));
+    }
 
-Auth::login($user);
-session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-return redirect()->route('users.show', [$user]);
-}
-public function edit(User $user)
-{
-$this->authorize('update', $user);
-return view('users.edit', compact('user'));
-}
-public function update(User $user, Request $request)
-{
-$this->authorize('update', $user);
-$this->validate($request, [
-'name' => 'required|max:50',
-'password' => 'nullable|confirmed|min:6'
-]);
-$data = [];
-$data['name'] = $request->name;
-if ($request->password) {
-$data['password'] = bcrypt($request->password);
-}
-$user->update($data);
-session()->flash('success', '个人资料更新成功！');
-return redirect()->route('users.show', $user);
-}
-}
+    public function create(){
+    return view('users.create');
+    }
+
+    public function show(User $user){
+    return view('users.show', compact('user'));
+    }
+
+    public function store(Request $request){
+    $this->validate($request, [
+    'name' => 'required|unique:users|max:50',
+    'email' => 'required|email|unique:users|max:255',
+    'password' => 'required|confirmed|min:6'
+    ]);
+
+    $user = User::create([
+    'name' => $request->name,
+    'email' => $request->email,
+    'password' => bcrypt($request->password),
+    ]);
+
+    Auth::login($user);
+    session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+    return redirect()->route('users.show', [$user]);
+    }
+
+    public function edit(User $user){
+    $this->authorize('update', $user);
+    return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request){
+    $this->authorize('update', $user);
+    $this->validate($request, [
+    'name' => 'required|max:50',
+    'password' => 'nullable|confirmed|min:6'
+    ]);
+    $data = [];
+    $data['name'] = $request->name;
+    if ($request->password) {
+    $data['password'] = bcrypt($request->password);
+    }
+    $user->update($data);
+    session()->flash('success', '个人资料更新成功！');
+    return redirect()->route('users.show', $user);
+        }
+    }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+    Model::unguard();
+    $this->call(UsersTableSeeder::class);
+    Model::reguard();
     }
 }
