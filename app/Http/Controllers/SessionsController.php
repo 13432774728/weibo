89834b370222a87e2c56a08ff0b94393a2d697b6
fcx <2196992498@qq.com>
2021-11-09 22:24:33 +0800

    用户激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -15,32 +16,35 @@ class SessionsController extends Controller
     'only' => ['create']
     ]);
     }
-public function create()
-{
-return view('sessions.create');
-}
-public function store(Request $request)
-{
-$credentials = $this->validate($request, [
-'email' => 'required|email|max:255',
-'password' => 'required'
-]);
-
-if (Auth::attempt($credentials)) {
-    //登录成功后的相关操作
-    session()->flash('success', '欢迎回来！');
-    $fallback = route('users.show', Auth::user());
-    return redirect()->intended($fallback);
+    public function create(){
+    return view('sessions.create');
+    }
+    
+    public function store(Request $request){
+    $credentials = $this->validate($request, [
+        'email' => 'required|email|max:255',
+        'password' => 'required'
+    ]);
+    
+    if (Auth::attempt($credentials, $request->has('remember'))) {
+    if(Auth::user()->activated) {
+        session()->flash('success', '欢迎回来！');
+        $fallback = route('users.show', Auth::user());
+        return redirect()->intended($fallback);
     } else {
-    //登录失败后的相关操作
+    Auth::logout();
+    session()->flash('warning', '你的账号未激活，请检查邮箱中的注册邮件进行激活。');
+        return redirect('/');
+    }
+    } else {
     session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-    return redirect()->back()->withInput();
+        return redirect()->back()->withInput();
     }
     }
-public function destroy()
-{
-Auth::logout();
-session()->flash('success', '您已成功退出！');
-return redirect('login');
-}
+    
+    public function destroy(){
+    Auth::logout();
+    session()->flash('success', '您已成功退出！');
+    return redirect('login');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable{
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
@@ -29,9 +29,17 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password','remember_token',
     ];
+
+    public static function boot(){
+        parent::boot();
+        
+        static::creating(function ($user) {
+            $user->activation_token = Str::random(10);
+        });
+        }
+
 
     /**
      * The attributes that should be cast.
@@ -41,11 +49,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    
-    public function gravatar($size = '100')
-    {
+
+    public function gravatar($size = '100'){
     $hash = md5(strtolower(trim($this->attributes['email'])));
     return " https://gravatar.zeruns.tech/avatar/$hash?s=$size";
     // return "http://www.gravatar.com/avatar/$hash?s=$size";
-    }
+    }
 }
