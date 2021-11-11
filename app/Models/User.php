9fb211167bca8b129fe8472dec2b18d27c1c9bd1
fcx <2196992498@qq.com>
2021-11-11 23:00:31 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
+use Auth;
 
 class User extends Authenticatable{
     use HasApiTokens, HasFactory, Notifiable;
@@ -63,8 +64,11 @@ class User extends Authenticatable{
 
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                             ->with('user')
+                            ->orderBy('created_at', 'desc');
     }
 
     public function followers(){
