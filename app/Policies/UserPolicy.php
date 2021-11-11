76c2d8909244240a2efdd45fc678bda8b5764ae0
fcx <2196992498@qq.com>
2021-11-11 22:56:21 +0800

    关注按钮

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -15,6 +15,10 @@ class UserPolicy{
     public function destroy(User $currentUser, User $user){
        return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
+    
+    public function follow(User $currentUser, User $user){
+        return $currentUser->id !== $user->id;
+    }
 }
 
 
