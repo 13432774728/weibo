de724ef2dcbef3334ac8a2f671f3e88af4a48a4d
fcx <2196992498@qq.com>
2021-11-11 20:20:52 +0800

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,7 +34,7 @@ class User extends Authenticatable{
 
     public static function boot(){
         parent::boot();
-        
+
         static::creating(function ($user) {
             $user->activation_token = Str::random(10);
         });
@@ -55,9 +55,15 @@ class User extends Authenticatable{
     return " https://gravatar.zeruns.tech/avatar/$hash?s=$size";
     // return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
-    
+
     public function statuses()
     {
     return $this->hasMany(Status::class);
-    }
+    }
+    
+    public function feed()
+    {
+        return $this->statuses()
+                    ->orderBy('created_at', 'desc');
+    }
 }
