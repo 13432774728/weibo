4de212e96d2f8643bff44e7a728684ec7a8bfe48
fcx <2196992498@qq.com>
2021-11-11 21:51:55 +0800

    删除微博

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -12,17 +12,25 @@ class StatusesController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     public function store(Request $request)
     {
         $this->validate($request, [
             'content' => 'required|max:140'
         ]);
-        
+
         Auth::user()->statuses()->create([
             'content' => $request['content']
         ]);
         session()->flash('success', '发布成功！');
         return redirect()->back();
+    }
+
+    public function destroy(Status $status)
+    {
+    $this->authorize('destroy', $status);
+    $status->delete();
+    session()->flash('success', '微博已被成功删除！');
+    return redirect()->back();
     }
 }
